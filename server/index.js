c0d69407127c3114f4ba48c80c5203062fec6239
krlim417 <krlim417@gmail.com>
2017-11-10 23:35:04 -0800

    Retrieves recommendations from API call successfully

--- server/index.js
+++ server/index.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mongo');
-// var request = require('request');
+var request = require('request');
+var config = require('../config');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// request.get()
+var options = {
+  q: '',
+  type: 'shows',
+  k: config.API_KEY
+};
+
+var joinedParameters = function(option) {
+  var joinedParams = 'https://tastedive.com/api/similar?';
+  for (var key in options) {
+    joinedParams += `${key}=${options[key]}`;
+    if (key !== 'k') {
+      joinedParams += '&';
+    }
+  }
+  return joinedParams;
+};
 
 app.post('/search', function (req, res) {
-  var searchValue = req.body.valueToFetch;
-  
-  res.status(200).send(searchValue);
+  options.q = req.body.valueToFetch;
+  request(joinedParameters(options), (err, data) => {
+    console.log('API CALL: ', data);
+  });
+  res.status(200).send(options.q);
 });
 
 app.listen(3000, function() {
