f46ae62bc1f579a1c3b128fa912d3dea5a084273
krlim417 <krlim417@gmail.com>
2017-11-12 02:21:53 -0800

    Fix table so that <tr> is not a child of <td>

--- react-client/src/components/TopFiveLiked.jsx
+++ react-client/src/components/TopFiveLiked.jsx
@@ -3,20 +3,23 @@ import React from 'react';
 const TopFiveLiked = (props) => (
   <div>
     <h4>The top liked in the database:</h4>
-    {console.log(props.top)}
     <table>
       <tbody>
         <tr>
         {props.top.map(item => {
           return (
             <td>
-              <tr>
-                <td>{item.name}</td>
-                <td>{item.likes}</td>
-              </tr>
-              <tr>
-                <td colSpan="2">image here</td>
-              </tr>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>{item.likes}</td>
+                  </tr>
+                  <tr>
+                    <td colSpan="2">image here</td>
+                  </tr>
+                </tbody>
+              </table>
             </td>
           );
         })}
