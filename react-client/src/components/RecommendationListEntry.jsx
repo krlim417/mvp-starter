f15284495a0049ce8aead3de866727ec619a38b9
krlim417 <krlim417@gmail.com>
2017-11-12 02:20:52 -0800

    Change button to be button tag to set value as the tv show name

--- react-client/src/components/RecommendationListEntry.jsx
+++ react-client/src/components/RecommendationListEntry.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
 
-const RecommendationListEntry = (props) => (
-  <div>
-    <table>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Likes</th>
-          <th></th>
-        </tr>
-        {props.items.map(item => {
-          return (
+class RecommendationListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    console.log('EVENT: ', event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
             <tr>
-              <td>{item.name}</td>
-              <td>{item.likes}</td>
-              <td>
-                <form>
-                  <input type='button' value='like' />
-                </form>
-              </td>
+              <th>Name</th>
+              <th>Likes</th>
+              <th></th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  </div>
-);
+            {this.props.items.map(item => {
+              return (
+                <tr>
+                  <td className='item-name' key={item.name}>{item.name}</td>
+                  <td>{item.likes}</td>
+                  <td><button type='button' value={item.name} onClick={this.handleClick}>Like</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+};
 
 export default RecommendationListEntry;
